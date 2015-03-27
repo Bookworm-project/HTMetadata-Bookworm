57c6f97f8478c108ca34dacbfbdb325418a8eb5b
mfall3 <mfall3@illinois.edu>
2015-03-27 12:29:26 -0500

    sets labels for ui components

--- guiOptions/clean.py
+++ guiOptions/clean.py
@@ -1,25 +1,49 @@
 import json
+import argparse
+import sys
 
 def main():
-    input_file="example-autogen.json" # Better as arg
 
-    indata = json.load(file(input_file))
+    # Get arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputFile", nargs="?", help="location of automatically generated options json file", default=sys.stdin)
+    args = parser.parse_args()
+
+    indata = json.load(open(args.inputFile, 'r'))
     # Dict for writing the updated options
     outdata = indata.copy()
     outdata['ui_components'] = []
 
-    marc_lang = open("MarcLanguageCodes.tsv", "r+")
-    lang_codes = dict([tuple(line.strip().split("\t")) for line in marc_lang])
-
     ui = indata['ui_components']
     for component in ui:
         name = component['name']
-        print name
+        print (name)
+
+        if name == 'lc_class':
+            component['name'] = "Class"
+        if name == 'lc_subclass':
+           component['name'] = "Subclass"
+        if name == 'fiction_nonfiction':
+            component['name'] = "Fiction/Nonfiction"
+        if name == 'genre':
+            component['name'] = "Genre"
         if name == 'languages':
-            print component.keys()
             component['name'] = "Language"
-            #for description in component['categorical']['descriptions']:
-                #print description
+        if name == 'format':
+            component['name'] = "Format"
+        if name == 'is_gov_doc':
+            component['name'] = "Government Document"
+        if name == 'page_count_bin':
+            component['name'] = "Page Count"
+        if name == 'word_count_bin':
+            component['name'] = "Word Count"
+        if name == 'publication_country':
+            component['name'] = "Publication Country"
+        if name == 'publication_state':
+            component['name'] = "Publication State"
+        if name == 'date_year':
+            component['name'] = "Publication Year"
+
         outdata['ui_components'] += [component]
 
 if __name__ == "__main__":
